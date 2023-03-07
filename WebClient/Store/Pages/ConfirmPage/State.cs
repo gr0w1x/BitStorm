dfb20414c26f256645f98f2b3a4b9401b6977b95
Rostyslav Suchkov <rostyslav.suchkov.gr0w1x@gmail.com>
2023-03-07 18:19:55 +0300

    fix redux confirm page state

--- WebClient/Store/Pages/ConfirmPage/State.cs
+++ WebClient/Store/Pages/ConfirmPage/State.cs
@@ -1,5 +1,7 @@
+using Fluxor;
 using WebClient.Store.Pages.SignPageBase;
 
 namespace WebClient.Store.Pages.ConfirmPage;
 
+[FeatureState]
 public record ConfirmPageState(): SignPageBaseState<ConfirmPageState>;
